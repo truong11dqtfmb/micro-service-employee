4d3b7a7c7cc4e9e6cba0f58d0a31d602738caa99
Trượng Đào <quoctruong11tv@gmail.com>
2023-06-15 16:50:51 +0700

    config gateway

--- api-gateway/src/main/java/com/dqt/apigateway/filter/AuthenticationFilter.java
+++ api-gateway/src/main/java/com/dqt/apigateway/filter/AuthenticationFilter.java
@@ -1,15 +1,19 @@
 package com.dqt.apigateway.filter;
 
 
-import com.dqt.apigateway.exception.CustomException;
 import com.dqt.apigateway.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Component
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
@@ -26,34 +30,68 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
         super(Config.class);
     }
 
+    public static class Config {
+    }
+
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//        return ((exchange, chain) -> {
+//            if (validator.isSecured.test(exchange.getRequest())) {
+//                //header contains token or not
+//                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+//                    throw new RuntimeException("missing authorization header");
+//                }
+//
+//                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+//                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+//                    authHeader = authHeader.substring(7);
+//                }
+//                try {
+////                    //REST call to AUTH service
+////                    template.getForObject("http://IDENTITY-SERVICE//validate?token" + authHeader, String.class);
+//                    Boolean flag =  jwtUtil.validateToken(authHeader);
+//
+//                    System.out.println(flag);
+//
+//                } catch (Exception e) {
+//                    System.out.println("invalid access...!");
+//                    throw new RuntimeException("un authorized access to application");
+//                }
+//            }
+//            return chain.filter(exchange);
+//        });
+//    }
+
     @Override
     public GatewayFilter apply(Config config) {
-        return ((exchange, chain) -> {
-            if (validator.isSecured.test(exchange.getRequest())) {
-                //header contains token or not
-                if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
-                    throw new CustomException("missing authorization header");
-                }
+        return (exchange, chain) -> {
 
-                String authHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
-                if (authHeader != null && authHeader.startsWith("Bearer ")) {
-                    authHeader = authHeader.substring(7);
-                }
-                try {
-//                    //REST call to AUTH service
-//                    template.getForObject("http://IDENTITY-SERVICE//validate?token" + authHeader, String.class);
-                    jwtUtil.validateToken(authHeader);
+            ServerHttpRequest httpRequest = exchange.getRequest();
 
-                } catch (Exception e) {
-                    System.out.println("invalid access...!");
-                    throw new CustomException("un authorized access to application");
-                }
+            if(!httpRequest.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No Authorization Header", HttpStatus.UNAUTHORIZED);
             }
+//
+//            String authorizationHeader = httpRequest.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+            String authorizationHeader = exchange.getRequest().getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+
+            authorizationHeader = authorizationHeader.substring(7);
+
+            String jwt = authorizationHeader.replace("Bearer", "");
+
+            if(!jwtUtil.validateToken(jwt)) {
+                return onError(exchange, "JWT Token is not valid!", HttpStatus.UNAUTHORIZED);
+            }
+
             return chain.filter(exchange);
-        });
+        };
     }
 
-    public static class Config {
-
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus){
+        ServerHttpResponse httpResponse = exchange.getResponse();
+        httpResponse.setStatusCode(httpStatus);
+        return httpResponse.setComplete();
     }
+
+
 }
--- api-gateway/src/main/java/com/dqt/apigateway/util/JwtUtil.java
+++ api-gateway/src/main/java/com/dqt/apigateway/util/JwtUtil.java
@@ -1,15 +1,11 @@
 package com.dqt.apigateway.util;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 public class JwtUtil {
@@ -18,8 +14,14 @@ public class JwtUtil {
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
 
-    public void validateToken(final String token) {
-        Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    public Boolean validateToken(final String token) {
+        boolean returnValue = true;
+
+        String subject =  Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token).getBody().getSubject();
+        if(subject == null || subject.isEmpty()) {
+            returnValue = false;
+        }
+        return returnValue;
     }
 
 
